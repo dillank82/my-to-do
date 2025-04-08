081f1a36a8eb3420eb2410c4d8f9dee1aca610ce
dillank82 <dillankyuppi@gmail.com>
2025-04-08 12:42:33 +0500

    add separation into backend and frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react'
+import './App.css'
+import React from 'react'
+
+function App(): ReactNode {
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
